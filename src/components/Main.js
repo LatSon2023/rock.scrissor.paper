e9d5d81402dad7baaa1ecd3624ac690aaf893f43
LatSon2023 <2023newbie>
2023-02-24 12:10:11 +0700

    fix 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -47,13 +47,13 @@ export default function Main() {
     <div className="conainer">
       <div className="main">
         <Result
-          user1GameItem={"Your code here"}
-          user2GameItem={"Your code here"}
-          result={"Your code here"}
+          user1GameItem={userGameItem}
+          user2GameItem={computerGameItem}
+          result={result}
         />
         <Display />
         <Choices
-          gameItems={"Your code here"}
+          gameItems={gameItems}
           handleGameItemChange={"Your code here"}
         />
       </div>
